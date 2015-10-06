ef51b7722a41e459126f861a7bc0b3c6703b8180
Pete Wilcox <petercwilcox@gmail.com>
2015-10-05 20:15:28 -0700

    start on arraylist

--- lab2a/src/lab2a/Zip.java
+++ lab2a/src/lab2a/Zip.java
@@ -5,6 +5,7 @@ public class Zip {
     public static void main(String[] args) {
 
         zipperIntTester();
+        zipperListTester();
     }
 
     public static void zipperIntTester() {
@@ -116,4 +117,44 @@ public class Zip {
         }
         return zipped;
     }
+    
+        public static int[] zipperList(ArrayList<Integer> left, ArrayList<Integer> right) {
+        if (left == null && right == null) {
+            int[] zipped = new int[0];
+            return zipped;
+        } else if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        }
+
+        int[] zipped = new int[left.length + right.length];
+
+        int j = 0, k = 0;
+
+        for (int i = 0; i < zipped.length; i++) {
+            if (i % 2 == 0) {
+                // grab from left
+                if (j < left.length) {
+                    zipped[i] = left[j];
+                    j++;
+                } else {
+                    zipped[i] = right[k];
+                    k++;
+                }
+
+            } else {
+                // grab from right
+                if (k < right.length) {
+                    zipped[i] = right[k];
+                    k++;
+                } else {
+                    zipped[i] = left[j];
+                    j++;
+                }
+            }
+        }
+        return zipped;
+    }
+
 }
