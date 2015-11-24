3e6df94ef42ad2e19d1cf8f99e0a2bd0ec0e1e2e
Pete Wilcox <petercwilcox@gmail.com>
2015-11-23 19:31:22 -0800

    adding adventure framework

---
+++ adventure/src/adventure/Adventure.java
@@ -0,0 +1,24 @@
+/*
+ * adventure
+ * adventure
+ * Adventure.java
+ * Created by Pete Wilcox on Nov 23, 2015
+ * petercwilcox@gmail.com
+ * Copyright © 2015 Pete Wilcox. All rights reserved.
+ */
+package adventure;
+
+/**
+ *
+ * @author Pete
+ */
+public class Adventure {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
